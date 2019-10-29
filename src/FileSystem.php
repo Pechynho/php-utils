50d256d54f2832aa5739b8a39fcb46b7f325c362
Jan Pech <pechynho@gmail.com>
2019-10-29 12:56:47 +0100

    FileSystem::apend fix #3

--- src/FileSystem.php
+++ src/FileSystem.php
@@ -103,7 +103,7 @@ class FileSystem
 		{
 			throw new InvalidArgumentException("Given value '$filename' is not valid filename.");
 		}
-		$isEmpty = !FileSystem::isFile($filename) || FileSystem::isEmpty($filename);
+		$isEmpty = !(FileSystem::isFile($filename) && FileSystem::size($filename) > 0);
 		$file = fopen($filename, "a");
 		if ($isEmpty)
 		{
